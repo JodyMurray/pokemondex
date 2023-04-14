ef10b024620b5a33f98d6edda0db4093919ce1da
Jody Murray <jodymurray@Jodys-MacBook-Air-2.local>
2023-04-14 14:22:21 +0200

    Fix display for stats

--- pokemondex/src/App.js
+++ pokemondex/src/App.js
@@ -5,19 +5,29 @@ import Axios from 'axios';
 function App() {
 
   const [pokemonName, setPokemonName] = useState('');
-  const [pokemon, setPokemon] = useState({})
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defence: "",
+    type: "",
+  })
   const searchPokemon = () => {
     Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
       setPokemon({
-        name: pokemonName, 
-        species: response.data.species.name, 
-        img: response.data.sprites.front_default, 
-        hp: response.data.stats[0].base_stat, 
+        name: pokemonName,
+        species: response.data.species.name,
+        img: response.data.sprites.front_default,
+        hp: response.data.stats[0].base_stat,
         attack: response.data.stats[1].base_stat,
         defence: response.data.stats[2].base_stat,
         type: response.data.types[0].type.name,
 
-      })
+      });
+      setPokemonChosen(true);
     });
   };
 
@@ -33,6 +43,19 @@ function App() {
           setPokemonName(event.target.value);
         }} />
         <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className='DisplaySection'>
+        {!pokemonChosen ? (<h2> Please choose a Pokemon</h2>
+        ) : (
+        <><h2>{pokemon.name}</h2>
+        <img src={pokemon.img} />
+        <h3>Species: {pokemon.species}</h3>
+        <h3>Type: {pokemon.type}</h3>
+        <h4>HP: {pokemon.hp}</h4>
+        <h4>Attack: {pokemon.attack}</h4>
+        <h4>Defence: {pokemon.defence}</h4>
+        </>
+        )}
       </div>
     </div>
   );
