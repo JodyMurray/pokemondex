351b915a81316f3fb1a89dabe6f067e977a6ab20
Jody Murray <jodymurray@Jodys-MacBook-Air-2.local>
2023-04-14 13:57:57 +0200

    Create axios response data

--- pokemondex/src/App.js
+++ pokemondex/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
 
   const [pokemonName, setPokemonName] = useState('');
+  const [pokemon, setPokemon] = useState({})
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => {
+      setPokemon({
+        name: pokemonName, 
+        species: response.data.species.name, 
+        img: response.data.sprites.front_default, 
+        hp: response.data.stats[0].base_stat, 
+        attack: response.data.stats[1].base_stat,
+        defence: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+
+      })
+    });
+  };
+
+
 
   return (
     <div className="App">
@@ -14,7 +32,7 @@ function App() {
         <input type="text" onChange={(event) => {
           setPokemonName(event.target.value);
         }} />
-        <button>Search Pokemon</button>
+        <button onClick={searchPokemon}>Search Pokemon</button>
       </div>
     </div>
   );
